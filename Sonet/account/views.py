5f352af76569141b360573ad222ff35df0449a1d
sanix sandel <sanicksikani@gmail.com>
2020-07-06 20:47:00 +0300

    adding social auth

--- Sonet/account/views.py
+++ Sonet/account/views.py
@@ -8,6 +8,7 @@ from .forms import (
     ProfileEditForm) 
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 @login_required
 def dashboard(request):
@@ -73,6 +74,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')   
     else:
         user_form=UserEditForm(instance=request.user)
         profile_form=ProfileEditForm(instance=request.user.profile)
